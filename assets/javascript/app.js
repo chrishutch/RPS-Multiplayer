3de8a881b523220a8f0c36df102c6bba720c33df
Chris Hutchinson <chrisbond19@me.com>
2018-01-20 23:46:12 -0600

    You can add players' name and will get added to database.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,85 @@
 $(document).ready(function() {
 	  // Initialize Firebase
     var config = {
-      apiKey: "AIzaSyAAHDR8xsv0bqaZ1p_1pCQduDtwE4BFWPM",
-      authDomain: "train-scheduler-6a152.firebaseapp.com",
-      databaseURL: "https://train-scheduler-6a152.firebaseio.com",
-      projectId: "train-scheduler-6a152",
+      apiKey: "AIzaSyA8lYZp2wKygWv5Yy5JmBPQ__LnGsbFvhM",
+      authDomain: "rps-multiplayer-fbe94.firebaseapp.com",
+      databaseURL: "https://rps-multiplayer-fbe94.firebaseio.com",
+      projectId: "rps-multiplayer-fbe94",
       storageBucket: "",
-      messagingSenderId: "642288717061"
+      messagingSenderId: "441560983773"
     };
     firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  // 2. Button for adding trains
-  $("#submit-player").on("click", function(event) {
-    event.preventDefault();
+  // Variables
+  var players;
+  var player1;
+  var player2;
+  var wins1 = 0;
+  var wins2 = 0;
+  var losses1 = 0;
+  var losses2 = 0;
+  var choice1;
+  var choice2;
+  var name1;
+  var name2;
+  var choices = ['Rock', 'Paper', 'Scissors'];
+  var chosenplayer = 0;
+console.log(chosenplayer);
 
-    // Grabs user input
-    var playerName = $("#player-name").val().trim();
+    // 2. Button for adding trains
+    $("#submit-player").on("click", function(event) {
+      event.preventDefault();
+if (chosenplayer == 0){
+      // Grabs user input
+      player1 = $("#player-name").val().trim();
 
-    // Creates local "temporary" object for holding train data
-    var newPlayer = {
-      name: playerName,
-    };
+      // Creates local "temporary" object for holding train data
+      players = {
+        name_1: player1,
+        // choice: choice1,
+        // wins: wins1,
+        // losses: losses1
+      };
 
-    // Uploads employee data to the database
-    database.ref().push(newPlayer);
+      // Uploads employee data to the database
+      database.ref().push(players);
 
-    // Logs everything to console
-    console.log(newPlayer.name);
+      // Logs everything to console
+      console.log(players);
 
-    // Clears all of the text-boxes
-    $("#player-name").val("");
-  });
+      // Clears all of the text-boxes
+      $("#player-name").val("");
+      chosenplayer++;
+      $('#player-1').text(player1);
+      console.log(chosenplayer);
+    
+  }
+  else if(chosenplayer == 1){
 
+      // Grabs user input
+      player2 = $("#player-name").val().trim();
+
+      // Creates local "temporary" object for holding train data
+      players = {
+        name_2: player2,
+        // choice: choice2,
+        // wins: wins2,
+        // losses: losses2
+      };
+
+      // Uploads employee data to the database
+      database.ref().push(players);
+
+      // Logs everything to console
+      console.log(players);
+
+      // Clears all of the text-boxes
+      $("#player-name").val("");
+      $('#player-2').text(player2);
+  }
+});
   // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
 //   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
